3dc66dd0d28d04a2e439171bbc1c694ed0e3e21a
Caceres Miguel <miguelcaceres94@hotmail.com>
2017-09-14 18:31:44 +0200

    Add MD5 hash generator sample

---
+++ src/js/md5Edit.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+    $('.user-input').on('keyup', function () {
+        if ($('.user-input').val().length > 0) {
+            // Get the value from the input and hash it
+            var hash = CryptoJS.MD5($('.user-input').val());
+            // Update the value of hash to get the new output on the screen
+            $('.' + $('.hash').attr('class')).val(hash.toString());
+        } else {
+            // clean the value if user delete all input value
+            $('.hash').val('');
+        }
+    });
+
+});
