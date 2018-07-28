623dc988aa547128020d221765a1b1a6ba1cd272
foxneo <mcaceres@uni-bremen.de>
2018-07-28 16:34:47 +0200

    Add hash sha-256 generator

--- README.md
+++ README.md
@@ -8,6 +8,10 @@ This is for educational purposes only
 
 * <a href="https://foxneo.github.io/CryptoProjectsJS/src/html/md5.html" target="_blank">live demo</a>
 
+## SHA-256 Hash generator demo ##
+
+* <a href="http://foxneo.github.io//CryptoProjectsJS/src/html/sha-256.html" target="_blank">live demo</a>
+
 ## SHA-3 Hash generator demo ##
 
 * <a href="http://foxneo.github.io//CryptoProjectsJS/src/html/sha-3.html" target="_blank">live demo</a>
---
+++ src/js/sha-256Edit.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+    $('.user-input').on('keyup', function () {
+        if ($('.user-input').val().length > 0) {
+            // Get the value from the input and hash it
+            let hash = CryptoJS.SHA256($('.user-input').val());
+            var hash2 = CryptoJS.SHA256("shivasurya");
+            console.log(hash2);
+            // Update the value of hash to get the new output on the screen
+            $('.' + $('.hash').attr('class')).val(hash.toString());
+        } else {
+            // clean the value if user delete all input value
+            $('.hash').val('');
+        }
+    });
+});
